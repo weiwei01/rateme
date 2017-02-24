e344c758dc1054fad01806f1bfda316bcdb25cd8
nunu <wi4213@gmail.com>
2017-02-24 22:12:24 +0800

    adding mongodb session store

--- server.js
+++ server.js
@@ -4,9 +4,12 @@ var bodyParser = require("body-parser");
 var ejs = require("ejs");
 var engine =require('ejs-mate');
 var session = require('express-session');
-
+var mongoose = require('mongoose');
+var MongoStore = require('connect-mongo')(session);
 
 var app = express();
+
+mongoose.connect('mongodb://liocalhost/rateme');
 
 app.use(express.static('public'));
 app.engine('ejs', engine);
@@ -15,6 +18,12 @@ app.use(cookieParser());
 app.use(bodyParser.urlencoded({extended:true}));
 app.use(bodyParser.json());
 
+app.use(session({
+  secret: 'Thisismytestkey',
+  resave: false,
+  saveUninitialized:false,
+  store: new MongoStore({mongooseConnection:mongoose.connection})
+}))
 
 app.get('/', function(req, res, next){
   res.render('index');
